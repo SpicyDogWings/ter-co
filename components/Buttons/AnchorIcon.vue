5739057ccc7d44365ffa642a436764a34ad5cba9
spicydogwings <spaceprotondev@proton.me>
2025-09-28 11:28:48 -0600

    fix(ux): área de click del ícono del botón

--- components/Buttons/AnchorIcon.vue
+++ components/Buttons/AnchorIcon.vue
@@ -3,8 +3,8 @@ const props = defineProps(["icon", "href"]);
 </script>
 
 <template>
-  <div class="w-8 h-6 transition-all duration-150 hover:cursor-pointer active:scale-90">
-    <a class="w-full h-full flex justify-center items-center " :href="props.href">
+  <div class="transition-all duration-150 hover:cursor-pointer active:scale-90">
+    <a class="w-8 h-6 flex justify-center items-center " :href="props.href">
       <Icon :name="props.icon" class="w-full h-full text-gray-500 hover:text-blue-500"></Icon>
     </a>
   </div>
